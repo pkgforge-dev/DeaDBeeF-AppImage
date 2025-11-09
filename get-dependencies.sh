ad90eb89488a134ec909f2c6a48f8e8639495557
Samuel <36420837+Samueru-sama@users.noreply.github.com>
2025-11-09 05:11:50 -0400

    Update get-dependencies.sh

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -30,6 +30,8 @@ wget --retry-connrefused --tries=30 "$EXTRA_PACKAGES" -O ./get-debloated-pkgs.sh
 chmod +x ./get-debloated-pkgs.sh
 ./get-debloated-pkgs.sh --add-common --prefer-nano
 
+pacman -Rsndd --noconfirm mesa
+
 echo "Gettign deadbeef..."
 echo "---------------------------------------------------------------"
 if [ "$DEVEL" = true ]; then
@@ -57,24 +59,9 @@ wget --retry-connrefused --tries=30 "$TARBALL" -O /tmp/download.tar.bz2
 tar xvf /tmp/download.tar.bz2
 mkdir -p ./AppDir
 mv -v ./deadbeef* ./AppDir/bin
+chmod +x ./AppDir/bin/*
 
 # remove all traces of gtk2
 find ./AppDir -type f -iname '*gtk2*'
 find ./AppDir -type f -iname '*gtk2*' -delete
 
-# add static binaries
-echo "Adding static bins..."
-FAAC_URL="https://pkgs.pkgforge.dev/dl/bincache/x86_64-linux/faac/nixpkgs/faac/raw.dl"
-FLAC_URL="https://pkgs.pkgforge.dev/dl/bincache/x86_64-linux/flac/nixpkgs/flac/raw.dl"
-LAME_URL="https://pkgs.pkgforge.dev/dl/bincache/x86_64-linux/lame/nixpkgs/lame/raw.dl"
-OGGENC_URL="https://pkgs.pkgforge.dev/dl/bincache/x86_64-linux/vorbis-tools/ppkg/stable/oggenc/raw.dl"
-OPUSENC_URL="https://pkgs.pkgforge.dev/dl/bincache/x86_64-linux/opus-tools/ppkg/stable/opusenc/raw.dl"
-WAVPACK_URL="https://pkgs.pkgforge.dev/dl/bincache/x86_64-linux/wavpack/nixpkgs/wavpack/raw.dl"
-
-#wget --retry-connrefused --tries=30 "$FAAC_URL"    -O  ./AppDir/bin/faac
-#wget --retry-connrefused --tries=30 "$FLAC_URL"    -O  ./AppDir/bin/flac
-#wget --retry-connrefused --tries=30 "$LAME_URL"    -O  ./AppDir/bin/lame
-#wget --retry-connrefused --tries=30 "$OGGENC_URL"  -O  ./AppDir/bin/oggenc
-#wget --retry-connrefused --tries=30 "$OPUSENC_URL" -O  ./AppDir/bin/opusenc
-#wget --retry-connrefused --tries=30 "$WAVPACK_URL" -O  ./AppDir/bin/wavpack
-chmod +x ./AppDir/bin/*
