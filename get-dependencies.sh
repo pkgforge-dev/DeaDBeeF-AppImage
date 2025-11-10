d04b11a65f495b528f1b2b8ae4a21bbca5c60b7e
Samuel <36420837+Samueru-sama@users.noreply.github.com>
2025-11-09 20:25:53 -0400

    migrate to sharun (#17)

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -1,28 +1,68 @@
 #!/bin/sh
 
-set -eux
+set -e
 EXTRA_PACKAGES="https://raw.githubusercontent.com/pkgforge-dev/Anylinux-AppImages/refs/heads/main/useful-tools/get-debloated-pkgs.sh"
 
 echo "Installing dependencies..."
 echo "---------------------------------------------------------------"
 pacman -Syu --noconfirm \
-	base-devel          \
-	curl                \
-	git                 \
-	libxss              \
-	pipewire-audio      \
-	pipewire-jack       \
-	pulseaudio          \
-	pulseaudio-alsa     \
-	wget                \
-	xorg-server-xvfb    \
+	base-devel       \
+	curl             \
+	faac             \
+	flac             \
+	git              \
+	lame             \
+	libxss           \
+	musepack-tools   \
+	opus-tools       \
+	pipewire-audio   \
+	pipewire-jack    \
+	pulseaudio       \
+	pulseaudio-alsa  \
+	vorbis-tools     \
+	wavpack          \
+	wget             \
+	xorg-server-xvfb \
 	zsync
 
 echo "Installing debloated packages..."
 echo "---------------------------------------------------------------"
 wget --retry-connrefused --tries=30 "$EXTRA_PACKAGES" -O ./get-debloated-pkgs.sh
 chmod +x ./get-debloated-pkgs.sh
-./get-debloated-pkgs.sh --add-common --prefer-nano
+./get-debloated-pkgs.sh gtk3-mini gdk-pixbuf2-mini librsvg-mini opus-mini libxml2-mini
 
-echo "All done!"
+pacman -Rsndd --noconfirm mesa
+
+echo "Gettign deadbeef..."
 echo "---------------------------------------------------------------"
+if [ "$DEVEL" = true ]; then
+	echo "Making nightly release..."
+	SITE="https://sourceforge.net/projects/deadbeef/files/Builds/master/linux"
+else
+	echo "Making stable release..."
+	SITE=$(wget "https://sourceforge.net/projects/deadbeef/files/travis/linux" -O - \
+		| sed 's/[()",{} ]/\n/g' | grep -o 'https.*linux.*download$' \
+		| grep -vi 'master\|feature\|bugfix' | head -1 | sed 's|/download||')
+fi
+
+TARBALL=$(wget "$SITE" -O - | sed 's/[()",{} ]/\n/g' \
+	| grep -o "https.*linux.*$ARCH.tar.bz2.*download$" | head -1)
+
+if [ "$DEVEL" = true ]; then
+	export VERSION=$(wget "$SITE" -O - | sed 's/"/ /g' \
+		| grep "files_date" | grep -o "[0-9]\{4\}-[0-9]\{2\}-[0-9]\{2\}" | head -1)
+else
+	export VERSION=$(echo "$TARBALL" | awk -F'_' '{print $2; exit}')
+fi
+echo "$VERSION" > ~/version
+
+wget --retry-connrefused --tries=30 "$TARBALL" -O /tmp/download.tar.bz2
+tar xvf /tmp/download.tar.bz2
+mkdir -p ./AppDir
+mv -v ./deadbeef* ./AppDir/bin
+chmod +x ./AppDir/bin/*
+
+# remove all traces of gtk2
+find ./AppDir -type f -iname '*gtk2*'
+find ./AppDir -type f -iname '*gtk2*' -delete
+
---
+++ make-appimage.sh
@@ -0,0 +1,41 @@
+#!/bin/sh
+
+set -e
+
+ARCH="$(uname -m)"
+VERSION="$(cat ~/version)"
+SHARUN="https://raw.githubusercontent.com/pkgforge-dev/Anylinux-AppImages/refs/heads/main/useful-tools/quick-sharun.sh"
+
+if [ "$DEVEL" = true ]; then
+	APP=DeaDBeeF_Nightly
+	branch=nightly
+else
+	APP=DeaDBeeF
+	branch=latest
+fi
+
+export OUTPUT_APPIMAGE=1
+export ADD_HOOKS="self-updater.bg.hook"
+export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}|$branch|*$ARCH.AppImage.zsync"
+export OUTNAME="$APP"-"$VERSION"-anylinux-"$ARCH".AppImage
+export DESKTOP="https://raw.githubusercontent.com/DeaDBeeF-Player/deadbeef/master/deadbeef.desktop.in"
+export ICON="https://raw.githubusercontent.com/DeaDBeeF-Player/deadbeef/master/icons/scalable/deadbeef.svg"
+export DEPLOY_PIPEWIRE=1
+
+# ADD LIBRARIES
+wget --retry-connrefused --tries=30 "$SHARUN" -O ./quick-sharun
+chmod +x ./quick-sharun
+./quick-sharun \
+	./AppDir/bin/*    \
+	./AppDir/bin/*/*  \
+	/usr/bin/faac     \
+	/usr/bin/flac     \
+	/usr/bin/lame     \
+	/usr/bin/mpcenc   \
+	/usr/bin/oggenc   \
+	/usr/bin/opusenc  \
+	/usr/bin/wavpack
+
+mkdir -p ./dist
+mv -v ./*.AppImage* ./dist
+mv -v ~/version     ./dist
