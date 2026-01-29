aedbeca85f3428517ef5a589d25a5384aa7ae1ad
Samuel <36420837+Samueru-sama@users.noreply.github.com>
2026-01-28 21:25:11 -0400

    update to template

--- make-appimage.sh
+++ make-appimage.sh
@@ -1,31 +1,18 @@
 #!/bin/sh
 
-set -e
+set -eu
 
-ARCH="$(uname -m)"
-VERSION="$(cat ~/version)"
-SHARUN="https://raw.githubusercontent.com/pkgforge-dev/Anylinux-AppImages/refs/heads/main/useful-tools/quick-sharun.sh"
-
-if [ "$DEVEL" = true ]; then
-	APP=DeaDBeeF_Nightly
-	branch=nightly
-else
-	APP=DeaDBeeF
-	branch=latest
-fi
-
-export OUTPUT_APPIMAGE=1
+ARCH=$(uname -m)
+export ARCH
+export OUTPATH=./dist
 export ADD_HOOKS="self-updater.bg.hook"
 export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}|$branch|*$ARCH.AppImage.zsync"
-export OUTNAME="$APP"-"$VERSION"-anylinux-"$ARCH".AppImage
 export DESKTOP="https://raw.githubusercontent.com/DeaDBeeF-Player/deadbeef/master/deadbeef.desktop.in"
 export ICON="https://raw.githubusercontent.com/DeaDBeeF-Player/deadbeef/master/icons/scalable/deadbeef.svg"
 export DEPLOY_PIPEWIRE=1
 
-# ADD LIBRARIES
-wget --retry-connrefused --tries=30 "$SHARUN" -O ./quick-sharun
-chmod +x ./quick-sharun
-./quick-sharun \
+# Deploy dependencies
+quick-sharun \
 	./AppDir/bin/*    \
 	./AppDir/bin/*/*  \
 	/usr/bin/faac     \
@@ -36,6 +23,7 @@ chmod +x ./quick-sharun
 	/usr/bin/opusenc  \
 	/usr/bin/wavpack
 
-mkdir -p ./dist
-mv -v ./*.AppImage* ./dist
-mv -v ~/version     ./dist
+# Additional changes can be done in between here
+
+# Turn AppDir into AppImage
+quick-sharun --make-appimage
